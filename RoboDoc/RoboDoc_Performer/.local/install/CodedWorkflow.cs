147a9c04dcd4273159c0782d6f942881c4dbed4f
Geldrich <geldrichp@gmail.com>
2024-03-13 19:04:36 +0200

    am adaugat potofoliul

---
+++ RoboDoc/RoboDoc_Performer/.local/install/CodedWorkflow.cs
@@ -0,0 +1,12 @@
+using UiPath.CodedWorkflows;
+
+namespace SIIT_Template_Performer
+{
+    public partial class CodedWorkflow : CodedWorkflowBase
+    {
+        public CodedWorkflow()
+        {
+            _ = new System.Type[]{};
+        }
+    }
+}
